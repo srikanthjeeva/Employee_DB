d6fff76e56aa232cb9edaca0348a285499cc885a
Srikanth <sri.jjhero@gmail.com>
2011-06-01 15:45:08 +0530

    display date in show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def display_date(text)
+    return nil unless text
+    return text if text.is_a?(String)
+    text.strftime("%Y-%m-%d")
+  end
 end
